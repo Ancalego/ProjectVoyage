b450db98a6d9d4936f6322ec664c382dcfa55917
Sam <samuli.juppi@metropolia.fi>
2018-10-08 13:41:58 +0300

    backend progress 40%

--- ProjectVoyage/start.js
+++ ProjectVoyage/start.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style,indent */
 var express = require("express");
 var app = express();
+var mysql = require('mysql');
 
 
 app.get("/", function (req, res) {
@@ -8,12 +9,35 @@ app.get("/", function (req, res) {
 });
 
 app.get("/ok", function (req, res) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "0000",
+        database: "voyage"
+    });
+    con.connect(function(err){
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = 'SELECT password FROM users WHERE username = "' + req.query.username + '";';
+        console.log(sql);
+        con.query(sql, function (err, result){
+            if (err) throw err;
+
+            else if (req.query.password == result[0].password){
+                res.sendFile( __dirname + "\\" + "main.html");
+            }
+            else{
+                res.sendFile( __dirname + "\\" + "login.html");
+            }
+            console.log("Result: " + result[0].password);
+        });
+    });
+
     response = {
         username:req.query.username,
         password:req.query.password
     }
     console.log(response);
-    res.sendFile( __dirname + "\\" + "main.html");
     //res.end(JSON.stringify(response));
 });
 
